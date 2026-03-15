710e884e91bec3c81e3662e0aa030575f747f2db
AmaanZaidi101 <44523548+AmaanZaidi101@users.noreply.github.com>
2026-03-16 02:52:11 +0530

    feat: scaffold .NET 10 MVC solution

---
+++ OwaspTop10.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace OwaspTop10.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
